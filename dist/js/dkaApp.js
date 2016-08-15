79aabbcf8b2494707d135c3908893670e29bc328
emanuele bastianelli <emanuelebastianelli@emanueles-MacBook-Pro.local>
2016-08-15 18:12:45 +0100

    Http requests added

--- dist/js/dkaApp.js
+++ dist/js/dkaApp.js
@@ -16,15 +16,15 @@ angular.module('dkaApp', ['ui.bootstrap','swd.inspector-gadget','chart.js'
     
   }])
   
-.controller('dkaController', ['$scope','$interval','$sce', dkaController]);
+.controller('dkaController', ['$scope','$interval','$sce', '$http',dkaController]);
 
 
 
 
-function dkaController($scope,$interval,$sce){
+function dkaController($scope,$interval,$sce,$http){
 	
-	
-	
+	// at the startup, we should have something that try to contact the robot and the kb server
+
 	var myInt = 1;
 	$interval(updateRooms, 2000,10);
 	
@@ -32,6 +32,37 @@ function dkaController($scope,$interval,$sce){
 		return Math.ceil(Math.random()*10);
 	};
 	function updateRooms(){
+		
+		// GET KB STATUS
+		// var globalQuery = query to ask for everything: temp, hum, wifi, people, validity for every room
+		// $http({
+		//   method: 'GET',
+		//   url: 'http://10.229.170.105:8080/query', // url should be loaded from a conf file
+		//   headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+		//   params: {query: globalQuery}
+		// }).then(function successCallback(response) {
+		//		// parse query response and do stuff
+		//   }, function errorCallback(response) {
+		// 	  	alert("Problems while contacting the KB server");
+		//   });
+		
+		// Maybe this can be put in another method, that is called more frequently
+		// ASK ROBOT WHERE IT IS: to be used to update the position of the robot
+		// watchout, with Chrome there might be proble with the Access-Control-Allow-Origin
+		//     		$http({
+		//     		  method: 'GET',
+		//     		  url: 'http://10.229.170.105:5000/whereareyou', // url should be loaded from a conf file
+		//     		  headers: {'Content-Type': 'application/x-www-form-urlencoded',
+		// 		 	 			"Access-Control-Allow-Origin": "*"}
+		//     		}).then(function successCallback(response) {
+		//     		    // parse plan response
+		// var x = response.data.current_position.x;
+		// var y = response.data.current_position.y;
+		//   				alert("Current position at [" + x + "," + y + "]");
+		//     		  }, function errorCallback(response) {
+		//     			  $scope.radioModel = "Sorry, problem while sending the reuqest. ERROR " + response.status
+		//     		  });
+		
 		myInt = myInt- 0.1; ;
 		$scope.colors = ['rgba(0, 180, 48, '+myInt+')','rgba(0, 180, 48, '+myInt+')','rgba(0, 180, 48, '+myInt+')','rgba(0, 180, 48, '+myInt+')'];
 		// $scope.rooms.colors = ['rgba(0, 180, 48, '+myInt+')','rgba(0, 180, 48, '+myInt+')','rgba(0, 180, 48, '+myInt+')','rgba(0, 180, 48, '+myInt+')'];
@@ -56,9 +87,8 @@ function dkaController($scope,$interval,$sce){
 		$scope.invalid = myInt < 0.1 ;	
 	}
 	
-	 $scope.colors =  ['rgba(0, 180, 48, 1)','rgba(0, 180, 48, 1)','rgba(0, 180, 48, 1)','rgba(0, 180, 48, 1)'];
-	
-	
+	$scope.colors =  ['rgba(0, 180, 48, 1)','rgba(0, 180, 48, 1)','rgba(0, 180, 48, 1)','rgba(0, 180, 48, 1)'];
+
 	$scope.rooms = {
 		bar :  {
 			scales : {
@@ -102,9 +132,10 @@ function dkaController($scope,$interval,$sce){
     
 	
 	// TODO load them from somewhere else
+	// queries are not compliant with URLs
 	$scope.queries = [
 		{'name':'Query 1',
-		'value': 'SELECT ?room ?wifiSignal WHERE { graph ?expiryDateInMs { ?room robo:hasWiFiSignal ?wifiSignal.}.  graph ?static {?room a location:Activity. } } ORDER BY DESC(?t) LIMIT 1'},
+		'value': 'select ?room ?temp where {graph ?expiryDateInMs { VALUES(?room) { ( <http://data.open.ac.uk/kmi/location/Podium> ) (<http://data.open.ac.uk/kmi/location/MarkBucks>) } ?room <http://data.open.ac.uk/kmi/robo/hasTemperature> ?temp. } }'},
 		{ 'name': 'Query 2',
 		  'value' : 'SELECT ?room ?temp WHERE { graph ?expiryDateInMs { VALUES(?room) {(location:Room20 ) (location:Room22)}.  ?room robo:hasTemperature ?temp.  }}'
 	    }, 
@@ -121,30 +152,97 @@ function dkaController($scope,$interval,$sce){
 	
 	$scope.queryServer = function(){
 			
-			// send query to server
-			
-			// while server is busy 
-			   // show ... rotating stuff
-				
+			// PERFORM QUERY QUERY
+			// TODO apparently it does not always take $scope.radioModel
+			// or it doesn't update it when someone writes inside the textarea
+			// $http({
+			//   method: 'GET',
+			//   url: 'http://10.229.170.105:8080/query', // url should be loaded from a conf file
+			//   headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+			//   params: {query: $scope.radioModel}
+			// }).then(function successCallback(response) {
+			//		// parse query response
+			//	    // update the resutl table
+			//      // ask for the plan - plan()
+			//   }, function errorCallback(response) {
+			// 	  $scope.radioModel = "Sorry, problem while sending the reuqest. ERROR " + response.status
+			//   });
+		 
 			$scope.results = true;				
 			
-			// plan?
+			// we can ask if it's busy here
+			
 			plan();
 			// then last query
 			
 			function plan(){
-		
+				
+			  	// GET PLAN
+				//   		$http({
+				//   		  method: 'GET',
+				//   		  url: 'http://10.229.170.105:8080/planner/plan', // url should be loaded from a conf file
+				//   		  headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+				//   		  params: {query: $scope.radioModel}
+				//   		}).then(function successCallback(response) {
+				//   		    // parse plan response
+				//				// do your stuff here
+				// alert(response.data);
+				//   		  }, function errorCallback(response) {
+				//   			  $scope.radioModel = "Sorry, problem while sending the reuqest. ERROR " + response.status
+				//   		  });
+				
+				// this must be called inside the successCallback method of the GET PLAN
+			  	// SEND QUERY FOR PLAN
+				// shouldn't the send be a POST????
+				// $http({
+				//   	method: 'GET',
+				//   	url: 'http://10.229.170.105:8080/bot/send', // url should be loaded from a conf file
+				//   	headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+				//   	params: {query: $scope.radioModel}
+				//  }).then(function successCallback(response) {
+				//   		    // parse plan response
+				// 			// do your stuff here
+				// 	alert(response.data);
+				//  }, function errorCallback(response) {
+				//  	$scope.radioModel = "Sorry, problem while sending the reuqest. ERROR " + response.status
+				//  });
+				//
+				
+				// also all this stuff should be called inside the successCallback method of the GET PLAN
+				// parsing the plan 
 				var plan = [{name : 'Go', executed : false,  executing : false}, {name : 'To' , executed : false,  executing : false}, {name : "The",  executed : false,  executing : false}, {name: "Kitchen", executed : false,  executing : false}];
 		
 				$scope.plan = plan;
 
 				var index = 0;
 				var lastAction = '';
+				
 				$interval(callAtInterval, 2000, $scope.plan.length+1);
 		
 		
 				function callAtInterval() {
-			
+					
+					// ASK ROBOT WHAT ARE YOU DOING: to be used to update the position of the robot
+					// watchout, with Chrome there might be proble with the Access-Control-Allow-Origin
+					// $http({
+					// 		method: 'GET',
+					// 		url: 'http://10.229.170.105:5000/do', // url should be loaded from a conf file
+					// 		headers: {'Content-Type': 'application/x-www-form-urlencoded',
+					// 			 	 	"Access-Control-Allow-Origin": "*"}
+					// 	}).then(function successCallback(response) {
+					// 		if (response.status == 204) {
+					// 			// doing nothing
+					// 			alert(response.statusText); //placeholder
+					// 		}
+					// 		else {
+					// 			// TODO we should put an id/counter in the plan so that it can more easily know
+					// 			// at which point of the global plan it is
+					// 			alert(response.data.name); //placeholder
+					// 		}
+					// 	}, function errorCallback(response) {
+					// 	    $scope.radioModel = "Sorry, problem while sending the reuqest. ERROR " + response.status
+					// });
+					
 					if (lastAction != '' ){
 						lastAction.executed = true;
 						lastAction.executing = false;
