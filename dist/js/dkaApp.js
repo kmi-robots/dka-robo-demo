ed7a481a54bbf9150efe63cabec4061f6b5194c9
emanuele bastianelli <emanuelebastianelli@emanueles-MacBook-Pro.local>
2016-08-18 17:12:14 +0100

    enabled the time validity transparency changing

--- dist/js/dkaApp.js
+++ dist/js/dkaApp.js
@@ -18,77 +18,119 @@ angular.module('dkaApp', ['ui.bootstrap','swd.inspector-gadget','chart.js'
   
 .controller('dkaController', ['$scope','$interval','$sce', '$http',dkaController]);
 
-
-
-
 function dkaController($scope,$interval,$sce,$http){
 	
+	// loading the property validity time file
+	// TODO: this should be replaced with a call to the server that
+	// serves this information
+	$scope.propertyvalidity = null;
+	$http.get('propertyValidity2.json')
+		.success(function(data) { 
+			$scope.propertyvalidity = data;   	
+		})
+	  	.error(function(data,status,error,config){
+			alert("Cannot load time validity: " + error);
+		})
+	
 	// at the startup, we should have something that try to contact the robot and the kb server
-
+	
 	var myInt = 1;
-	$interval(updateRooms, 2000,10);
+	updateRooms();
+	$interval(cycle, 2000);
 	
 	function random(){
 		return Math.ceil(Math.random()*10);
 	};
+	
+	function getURIName(uri) {
+		return uri.substr(uri.lastIndexOf('/')+1)
+	}
+
+	function cycle() {
+		updateRooms();
+	}
+	
 	function updateRooms(){
+		var updateQuery = "SELECT ?room ?validityGraph ?prop ?val where { graph ?validityGraph { ?room ?prop ?val } . graph <http://data.open.ac.uk/kmi/graph/static> {?room a <http://data.open.ac.uk/kmi/robo/Location> } . FILTER (?validityGraph != <http://data.open.ac.uk/kmi/graph/static> ) } ORDER BY ?room";
 		
 		// GET KB STATUS
 		// var globalQuery = query to ask for everything: temp, hum, wifi, people, validity for every room
-		// $http({
-		//   method: 'GET',
-		//   url: 'http://10.229.170.105:8080/query', // url should be loaded from a conf file
-		//   headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-		//   params: {query: globalQuery}
-		// }).then(function successCallback(response) {
-		//		// parse query response and do stuff
-		//   }, function errorCallback(response) {
-		// 	  	alert("Problems while contacting the KB server");
-		//   });
-		
+		$http({
+			method: 'GET',
+			url: 'http://10.229.170.105:8080/query', // url should be loaded from a conf file
+			headers: {'Content-Type': 'application/x-www-form-urlencoded', 'Accept':'application/json'},
+			params: {query: updateQuery}
+		}).then(function successCallback(response) {
+			var vars = response.data.vars;
+			var items = response.data.items;		
+			var now = Date.now();
+			
+			angular.forEach(items,function(item) {
+				
+				var room = getURIName(item.room);
+				//console.log(room);
+				var property = getURIName(item.prop);
+				var isValidUntil = parseInt(getURIName(item.validityGraph));
+				
+				var value = getURIName(item.val);
+				
+				if(item.val.includes("^^")) {
+					value = item.val.split("\^\^")[0];
+				}
+				
+				var remainingTime = isValidUntil - now;
+				var percentageElapsedTime = remainingTime/($scope.propertyvalidity[room][property]*1000);
+				
+				// this should never happen. 
+				// only for debugging
+				if(percentageElapsedTime > 1) {
+					percentageElapsedTime = 1;
+				}
+
+				var transparcencyValue = percentageElapsedTime;
+				var curRoom = null;
+				
+				for (index in $scope.rooms.rooms) {
+					var roomS = $scope.rooms.rooms[index];
+					if(room == roomS.name.replace(" ","")) {			
+						curRoom = roomS;
+						break;
+					}
+				}
+				if(curRoom != null) {
+					if(remainingTime > 0) {
+						curRoom.properties[property].color = 'rgba(0, 180, 48, '+transparcencyValue+')';
+						curRoom.properties[property].value = value;
+					}
+					else {
+						curRoom.properties[property].validity = false;
+						curRoom.properties[property].value = "NV";
+					
+					}
+				}
+			});
+		}, function errorCallback(response) {
+				alert("Problems while contacting the KB server");
+		});
+
 		// Maybe this can be put in another method, that is called more frequently
 		// ASK ROBOT WHERE IT IS: to be used to update the position of the robot
 		// watchout, with Chrome there might be proble with the Access-Control-Allow-Origin
-		//     		$http({
-		//     		  method: 'GET',
-		//     		  url: 'http://10.229.170.105:5000/whereareyou', // url should be loaded from a conf file
-		//     		  headers: {'Content-Type': 'application/x-www-form-urlencoded',
-		// 		 	 			"Access-Control-Allow-Origin": "*"}
-		//     		}).then(function successCallback(response) {
-		//     		    // parse plan response
-		// var x = response.data.current_position.x;
-		// var y = response.data.current_position.y;
-		//   				alert("Current position at [" + x + "," + y + "]");
-		//     		  }, function errorCallback(response) {
-		//     			  $scope.radioModel = "Sorry, problem while sending the reuqest. ERROR " + response.status
-		//     		  });
-		
-		myInt = myInt- 0.1; ;
-		$scope.colors = ['rgba(0, 180, 48, '+myInt+')','rgba(0, 180, 48, '+myInt+')','rgba(0, 180, 48, '+myInt+')','rgba(0, 180, 48, '+myInt+')'];
-		// $scope.rooms.colors = ['rgba(0, 180, 48, '+myInt+')','rgba(0, 180, 48, '+myInt+')','rgba(0, 180, 48, '+myInt+')','rgba(0, 180, 48, '+myInt+')'];
-		
-		oldRooms = $scope.rooms.rooms;
-		newRooms = [];
-		for (var roomIx = 0 ; roomIx < oldRooms.length; roomIx++){
-			room = oldRooms[roomIx];
-			var newData = [];
-			for (var value = 0 ; value < room.data.length ; value++){
-				
-				if (room.data[value] > 0) {
-					newVal = Math.floor(room.data[value]-1);
-				} else { newVal = 0;}
-				newData[value] = newVal;
-			}
-			room.data = newData;
-			$scope.rooms.rooms[roomIx].data =  newData;
-			
-		}
-		
-		$scope.invalid = myInt < 0.1 ;	
+		// $http({
+		//     method: 'GET',
+		//     url: 'http://10.229.170.105:8080/bot/wru', // url should be loaded from a conf file
+		//     headers: {'Content-Type': 'application/x-www-form-urlencoded',
+		// 		 	 "Access-Control-Allow-Origin": "*"}
+		//     	}).then(function successCallback(response) {
+		//     		 // parse plan response
+		// 			var x = response.data.current_position.x;
+		// 			var y = response.data.current_position.y;
+		// 			console.log("[ROBOT] Robot is @ x:" + x + " y:" + y)
+		//     	}, function errorCallback(response) {
+		//     		alert("[ROBOT] Sorry, the robot is not responding")
+		//     	});
 	}
 	
-	$scope.colors =  ['rgba(0, 180, 48, 1)','rgba(0, 180, 48, 1)','rgba(0, 180, 48, 1)','rgba(0, 180, 48, 1)'];
-
 	$scope.rooms = {
 		bar :  {
 			scales : {
@@ -112,20 +154,17 @@ function dkaController($scope,$interval,$sce,$http){
 					
 					pointLabels : {}
 				}
-		
 			},
-		colors :  ['rgba(0, 180, 48, 1)','rgba(0, 180, 48, 1)','rgba(0, 180, 48, 1)','rgba(0, 180, 48, 1)'],
-		data : [1,1,1,1],
-		labels : ['Temp','Hum','Wifi','Ppl'], 
+
 		rooms : [
-			{'name':'Room 22',data : [random(),random(),random(),random()], "left": '357px', top: '654px','w': '58px', 'h':'85px'},
-			{'name':'Room 20', data : [random(),random(),random(),random()], "left": '173px', top: '654px','w': '126px', 'h':'79px'},
-			{'name':'Markbucks',data : [random(),random(),random(),random()],  "left": '778px', top: '644px','w': '101px', 'h':'150px'},
-			{'name':'Podium', data : [random(),random(),random(),random()], "left": '841px', top: '262px','w': '244px', 'h':'274px'},
-			{'name':'Activity 2',data : [random(),random(),random(),random()], "left": '61px', top: '248px','w': '80px', 'h':'120px'},
-			{'name':'Activity 3',data : [random(),random(),random(),random()],"left": '221px', top: '248px','w': '80px', 'h':'120px'},
-			{'name':'Activity 4',data : [random(),random(),random(),random()],"left": '391px', top: '208px','w': '80px', 'h':'120px'},
-			{'name':'Activity 5',data : [random(),random(),random(),random()],"left": '551px', top: '208px','w': '80px', 'h':'120px'}
+			{'name':'Room 22', "left": '357px', top: '654px','w': '58px', 'h':'85px', properties:{ 'hasTemperature':{'name':'temperature','value':0, 'color':'rgba(0, 180, 48, 1)', 'validity':true},'hasHumidity':{'name':'humidity','value':0, 'color':'rgba(0, 180, 48, 1)','validity':true},'hasWiFiSignal':{'name':'wifi','value':0, 'color':'rgba(0, 180, 48, 1)','validity':true},'hasPeopleCount':{'name':'people','value':0, 'color':'rgba(0, 180, 48, 1)','validity':true}}},
+			{'name':'Room 20', "left": '173px', top: '654px','w': '126px', 'h':'79px', properties:{ 'hasTemperature':{'name':'temperature','value':0, 'color':'rgba(0, 180, 48, 1)', 'validity':true},'hasHumidity':{'name':'humidity','value':0, 'color':'rgba(0, 180, 48, 1)','validity':true},'hasWiFiSignal':{'name':'wifi','value':0, 'color':'rgba(0, 180, 48, 1)','validity':true},'hasPeopleCount':{'name':'people','value':0, 'color':'rgba(0, 180, 48, 1)','validity':true}}},
+			{'name':'MarkBucks', "left": '778px', top: '644px','w': '101px', 'h':'150px', properties:{ 'hasTemperature':{'name':'temperature','value':0, 'color':'rgba(0, 180, 48, 1)', 'validity':true},'hasHumidity':{'name':'humidity','value':0, 'color':'rgba(0, 180, 48, 1)','validity':true},'hasWiFiSignal':{'name':'wifi','value':0, 'color':'rgba(0, 180, 48, 1)','validity':true},'hasPeopleCount':{'name':'people','value':0, 'color':'rgba(0, 180, 48, 1)','validity':true}}},
+			{'name':'Podium', "left": '841px', top: '262px','w': '244px', 'h':'274px', properties:{ 'hasTemperature':{'name':'temperature','value':0, 'color':'rgba(0, 180, 48, 1)', 'validity':true},'hasHumidity':{'name':'humidity','value':0, 'color':'rgba(0, 180, 48, 1)','validity':true},'hasWiFiSignal':{'name':'wifi','value':0, 'color':'rgba(0, 180, 48, 1)','validity':true},'hasPeopleCount':{'name':'people','value':0, 'color':'rgba(0, 180, 48, 1)','validity':true}}},
+			{'name':'Activity 2', "left": '61px', top: '248px','w': '80px', 'h':'120px', properties:{ 'hasTemperature':{'name':'temperature','value':0, 'color':'rgba(0, 180, 48, 1)', 'validity':true},'hasHumidity':{'name':'humidity','value':0, 'color':'rgba(0, 180, 48, 1)','validity':true},'hasWiFiSignal':{'name':'wifi','value':0, 'color':'rgba(0, 180, 48, 1)','validity':true},'hasPeopleCount':{'name':'people','value':0, 'color':'rgba(0, 180, 48, 1)','validity':true}}},
+			{'name':'Activity 3', "left": '221px', top: '248px','w': '80px', 'h':'120px', properties:{ 'hasTemperature':{'name':'temperature','value':0, 'color':'rgba(0, 180, 48, 1)', 'validity':true},'hasHumidity':{'name':'humidity','value':0, 'color':'rgba(0, 180, 48, 1)','validity':true},'hasWiFiSignal':{'name':'wifi','value':0, 'color':'rgba(0, 180, 48, 1)','validity':true},'hasPeopleCount':{'name':'people','value':0, 'color':'rgba(0, 180, 48, 1)','validity':true}}},
+			{'name':'Activity 4', "left": '391px', top: '208px','w': '80px', 'h':'120px', properties:{ 'hasTemperature':{'name':'temperature','value':0, 'color':'rgba(0, 180, 48, 1)', 'validity':true},'hasHumidity':{'name':'humidity','value':0, 'color':'rgba(0, 180, 48, 1)','validity':true},'hasWiFiSignal':{'name':'wifi','value':0, 'color':'rgba(0, 180, 48, 1)','validity':true},'hasPeopleCount':{'name':'people','value':0, 'color':'rgba(0, 180, 48, 1)','validity':true}}},
+			{'name':'Activity 5', "left": '551px', top: '208px','w': '80px', 'h':'120px', properties:{ 'hasTemperature':{'name':'temperature','value':0, 'color':'rgba(0, 180, 48, 1)', 'validity':true},'hasHumidity':{'name':'humidity','value':0, 'color':'rgba(0, 180, 48, 1)','validity':true},'hasWiFiSignal':{'name':'wifi','value':0, 'color':'rgba(0, 180, 48, 1)','validity':true},'hasPeopleCount':{'name':'people','value':0, 'color':'rgba(0, 180, 48, 1)','validity':true}}}
 		]
 	}
 
@@ -185,6 +224,7 @@ function dkaController($scope,$interval,$sce,$http){
 				//   		  params: {query: $scope.radioModel}
 				//   		}).then(function successCallback(response) {
 				//   		    // parse plan response
+				//				// update and/or 
 				//				// do your stuff here
 				// alert(response.data);
 				//   		  }, function errorCallback(response) {
@@ -226,7 +266,7 @@ function dkaController($scope,$interval,$sce,$http){
 					// watchout, with Chrome there might be proble with the Access-Control-Allow-Origin
 					// $http({
 					// 		method: 'GET',
-					// 		url: 'http://10.229.170.105:5000/do', // url should be loaded from a conf file
+					// 		url: 'http://10.229.170.105:8080/bot/doing', // url should be loaded from a conf file
 					// 		headers: {'Content-Type': 'application/x-www-form-urlencoded',
 					// 			 	 	"Access-Control-Allow-Origin": "*"}
 					// 	}).then(function successCallback(response) {
